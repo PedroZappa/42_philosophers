5df60b2eb4387720b50521d0828c8b1c58201c36
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-16 17:53:17 +0100

    philo/ft_error.c : norm check

--- philo/ft_error.c
+++ philo/ft_error.c
@@ -6,24 +6,24 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 15:49:16 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/16 16:07:50 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/16 17:52:53 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int ft_putstr_fd(char *str, int fd);
-static int ft_strlen(char *str);
+static int	ft_putstr_fd(char *str, int fd);
+static int	ft_strlen(char *str);
 
 /// @brief		Print error message
 /// @param err	Error message to print
-void	ft_perror(char *err)
+int	ft_perror(char *err)
 {
 	ft_putstr_fd(err, 2);
-	return ;
+	return (1);
 }
 
-static int ft_putstr_fd(char *str, int fd)
+static int	ft_putstr_fd(char *str, int fd)
 {
 	if (!str || (fd < 0))
 		return (0);
@@ -31,10 +31,10 @@ static int ft_putstr_fd(char *str, int fd)
 	return (0);
 }
 
-static int ft_strlen(char *str)
+static int	ft_strlen(char *str)
 {
 	int	len;
-	
+
 	len = 0;
 	while (str[len])
 		++len;
