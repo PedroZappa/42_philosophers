fae8fb84fcd3c09e4be701de2d351dadb94292c3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-16 17:42:46 +0100

    philo/ft_parse.c : basic parsing implemnented

---
+++ philo/ft_parse.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 17:15:10 by passunca          #+#    #+#             */
+/*   Updated: 2024/05/16 17:34:59 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	ft_atol(char *str);
+int		ft_isspace(char c);
+int		ft_isdigit(char c);
+
+void	ft_parse_args(t_data *dinner, char **argv)
+{
+	dinner->n_philos = ft_atol(argv[1]);
+	dinner->t_death = ft_atol(argv[2]);
+	dinner->t_meal = ft_atol(argv[3]);
+	dinner->t_sleep = ft_atol(argv[4]);
+	if (argv[5])
+		dinner->n_meals = ft_atol(argv[5]);
+	else
+		dinner->n_meals = -1;
+}
+
+long	ft_atol(char *str)
+{
+	long	num;
+	int		i;
+	int		sign;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (ft_isspace(str[i]))
+		++i;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			ft_perror("Negative number\n");
+			return (0);
+		}
+		++i;
+	}
+	while (ft_isdigit(str[i]))
+		num = (num * 10) + (str[i++] - '0');
+	return (num * sign);
+}
+
+int	ft_isspace(char c)
+{
+	if ((c == ' ') || (c == '\t') || (c == '\n')
+		|| (c == '\v') || (c == '\f') || (c == '\r'))
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_isdigit(char c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
+}
