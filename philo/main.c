c84eef3cabb777ce246f4a6784963a432384da35
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-17 17:43:19 +0100

    philo/main.c : cleanup

--- philo/main.c
+++ philo/main.c
@@ -6,40 +6,33 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:17:35 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/16 17:56:43 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:37:54 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	ft_philosophize(t_data *dinner);
+static void	ft_philosophize(t_philo *philos);
 // static void	*ft_monitor(void *args);
 
-// 1. Parse Input => Error checking && Pre-initialization
-//	== Is the input an int?
-//	== Is it larger than INT_MAX?
-//	== Is timestamps > 60ms?
-//	/// atol() and conversion to msec
-// 2. Initialize Data => Forks, Philos
-// 3. Start Threads => Dinner time!
-// 4. Cleanup => When all philos have eaten || a philo has died
+/// @brief		Main function
+/// @param argc	Number of arguments
+/// @param argv	Argument vector
 int	main(int argc, char **argv)
 {
-	t_data	dinner;
+	t_philo	*philos;
 
 	if ((argc != 5) && (argc != 6))
 		return (ft_perror(RED"Wrong number of arguments\n"NC));
-	else
-	{
-		ft_parse_args(&dinner, argv);
-		ft_philosophize(&dinner);
+	if (ft_init(&philos, argc, argv) == -1)
 		return (0);
-	}
+	ft_philosophize(philos);
+	return (0);
 }
 
-static void	ft_philosophize(t_data *dinner)
+static void	ft_philosophize(t_philo *philos)
 {
-	(void)dinner;
+	(void)philos;
 }
 
 // static void	*ft_monitor(void *args)
