5ef5b4da6f22c151d8ccec219cba4e1fcddb62c9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-17 18:27:29 +0100

    philo/ft_init.c : commented

--- philo/ft_init.c
+++ philo/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 17:33:44 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/17 18:17:53 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/17 18:27:09 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,18 @@ static int	ft_init_data(t_data **data, int argc, char **argv);
 static int	ft_init_fork(t_data *data);
 static void	ft_init_philo(t_philo *philo, int i, t_data *data, t_mutex *forks);
 
+/// @brief			Initialize all simulation data
+/// @details		- Initialize mutexes
+///					- Initialize forks
+///					- Alloc & Initialize philos
+/// @param philo	Pointer to array of philos
+/// @param argc		Number of arguments
+/// @param argv		Argument vector
+/// @return			0 on success, -1 on failure
 int	ft_init(t_philo **philo, int argc, char **argv)
 {
 	t_data	*data;
-	t_philo	*tmp;
+	t_philo	*new_philo;
 	int		i;
 
 	if (ft_init_data(&data, argc, argv) == -1)
@@ -29,13 +37,13 @@ int	ft_init(t_philo **philo, int argc, char **argv)
 		return (ft_perror(RED"Error: Printf Mutex init\n"NC));
 	if (ft_init_fork(data) == -1)
 		return (-1);
-	tmp = malloc(sizeof(t_philo) * data->n_philos);
-	if (!tmp)
+	new_philo = malloc(sizeof(t_philo) * data->n_philos);
+	if (!new_philo)
 		return (ft_perror(RED"Error: failed to alloc philos\n"NC));
 	i = -1;
 	while (++i < data->n_philos)
-		ft_init_philo((tmp + i), i, data, data->mutex_fork);
-	*philo = tmp;
+		ft_init_philo((new_philo + i), i, data, data->mutex_fork);
+	*philo = new_philo;
 	return (0);
 }
 
