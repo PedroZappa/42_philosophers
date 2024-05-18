3968c272517b3d1abb15f5f3d7564ac8a953e358
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-18 11:34:53 +0100

    philo/main.c : ft_philosophize implemnted

--- philo/main.c
+++ philo/main.c
@@ -13,7 +13,8 @@
 #include "philo.h"
 
 static void	ft_philosophize(t_philo *philos);
-// static void	*ft_monitor(void *args);
+static void	*ft_start_philo(void *args);
+static void	*ft_monitor(void *args);
 
 /// @brief		Main function
 /// @param argc	Number of arguments
@@ -30,13 +31,39 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
+/// @brief			Launch all philos
+/// @param philos	Pointer to array of philos
 static void	ft_philosophize(t_philo *philos)
 {
-	(void)philos;
+	int	i;
+
+	i = -1;
+	philos->data->t_start = ft_gettime();
+	while (++i < philos->data->n_philos)
+	{
+		(philos + i)->t_meal = ft_gettime();
+		if (pthread_create(&philos[i].thread, NULL, \
+			&ft_start_philo, &philos[i]) != 0)
+			ft_perror(RED"Error: Failed to create philo thread\n"NC);
+	}
+	if (pthread_create(&philos->data->monitor, NULL, &ft_monitor, philos) != 0)
+		ft_perror(RED"Error: Failed to create monitor thread\n"NC);
+	if (pthread_join(philos->data->monitor, NULL) != 0)
+		ft_perror(RED"Error: Failed to join monitor thread\n"NC);
+	i = -1;
+	while (++i < philos->data->n_forks)
+		pthread_mutex_destroy(&philos->data->mutex_fork[i]);
+	pthread_mutex_destroy(&philos->data->mutex_printf);
 }
 
-// static void	*ft_monitor(void *args)
-// {
-// 	(void)args;
-// 	return (NULL);
-// }
+static void	*ft_start_philo(void *args)
+{
+	(void)args;
+	return (NULL);
+}
+
+static void	*ft_monitor(void *args)
+{
+	(void)args;
+	return (NULL);
+}
