0a8d0ca4d80fc4d65a7a4b1d9e101008184455c0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-18 11:58:55 +0100

    philo/ft_time.c : ft_update_time() implemented & commented

--- philo/ft_time.c
+++ philo/ft_time.c
@@ -21,3 +21,22 @@ t_msec	ft_gettime(void)
 	gettimeofday(&t, NULL);
 	return ((t.tv_sec * 1000) + (t.tv_usec / 1000));
 }
+
+/// @brief			Wait for a certain amount of time
+/// @param time		Time to wait in milliseconds
+/// @param data		Pointer to t_data struct
+/// @details		- Get current time in milliseconds
+/// 				- Wait until time is reached or data->done is set
+///
+void	ft_update_time(t_msec time, t_data *data)
+{
+	t_msec	t;
+
+	t = ft_gettime();
+	while (!data->done)
+	{
+		if ((ft_gettime() - t) >= time)
+			break ;
+		usleep(500);
+	}
+}
