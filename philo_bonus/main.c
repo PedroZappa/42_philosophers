138bd3f1a4f5cafd70f65da3ef2f4b52acf19cec
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 22:48:03 +0100

    philo_bonus/main.c : ft_philosophize() implemented

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+#include <pthread.h>
 
 static void	ft_philosophize(t_philo *philo);
 static void	ft_free(t_philo **philo);
@@ -47,10 +48,44 @@ int	main(int argc, char **argv)
 
 static void	ft_philosophize(t_philo *philo)
 {
-	(void)philo;
-}
+	if (pthread_create(&philo->monitor, NULL, &ft_monitor, philo))
+		ft_perror("Error: pthread_create failed\n");
+	if (philo->idx % 2 == 1)
+		usleep(1000);
+	while (1)
+	{
+		ft_philo_log(philo, "is thinking");
+		sem_wait(philo->sem_forks);
+		ft_philo_log(philo, "has taken a fork");
+		sem_wait(philo->sem_forks);
+		ft_philo_log(philo, "is eating");
+		ft_philo_do(philo->t_meal, philo);
+		philo->t_meal = ft_gettime();
+		sem_post(philo->sem_forks);
+		sem_post(philo->sem_forks);
+		philo->curr_meal++;
+		ft_philo_log(philo, "is sleeping");
+		ft_philo_do(philo->t_sleep, philo);
+	}
+	if (pthread_join(philo->monitor, NULL))
+		ft_perror("Error: pthread_join failed\n");
 
-static void	ft_free(t_philo **philo)
-{
-	(void)philo;
 }
+//
+// ///
+// /// @brief			Lock and unlock mutex and print message to stdout
+// /// @param philo	Pointer to a t_philo struct
+// /// @param str		Message to print
+// void	ft_philo_log(t_philo *philo, char *str)
+// {
+// 	sem_wait(philo->sem_printf);
+// 	if (!philo->wait)
+// 		printf("%lld %d %s\n", \
+// 				(ft_gettime() - philo->t_start), philo->idx, str);
+// 	sem_post(philo->sem_printf);
+// }
+//
+// static void	ft_free(t_philo **philo)
+// {
+// 	(void)philo;
+// }
