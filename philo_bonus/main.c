7f0ee1cf337c7265ceb435ac0ed90f1747c01016
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 15:50:54 +0100

    philo_bonus/main.c : implemented

---
+++ philo_bonus/main.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 15:39:21 by passunca          #+#    #+#             */
+/*   Updated: 2024/05/23 15:46:22 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+int main(int argc, char **argv)
+{
+	t_data	data;
+	t_philo	*philo;
+
+	if ((argc == 5) || (argc == 6))
+	{
+		if (ft_parse(argv, &data))
+			return (1);
+		philo = ft_init_philo(&data);
+		data.t_start = ft_gettime();
+		ft_init_semaphores(&data);
+		sem_wait(data.stop);
+		ft_fork_process(&data, philo);
+		sem_wait(data.stop);
+		ft_free(&philo);
+	}
+	return (0);
+}
