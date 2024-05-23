c423837f8798e0ae94ac176f899df4571cb8e46c
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 17:02:29 +0100

    philo_bonus/ft_init.c : ft_init_philo() implemented

--- philo_bonus/ft_init.c
+++ philo_bonus/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:00:25 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/23 16:09:39 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:01:56 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 t_philo	*ft_init_philo(t_data *data)
 {
-	(void)data;
-	return (NULL);
+	t_philo	*philo;
+	int		i;
+
+	i = -1;
+	philo = malloc(sizeof(t_philo) * data->n_philos);
+	data->death = NULL;
+	data->stop = NULL;
+	data->msg = NULL;
+	data->forks = NULL;
+	while (++i < data->n_philos)
+	{
+		philo[i].idx = i;
+		philo[i].died = NO;
+		philo[i].data = data;
+		philo[i].pid = -1;
+		if (data->meal_counter == -1)
+			philo[i].meal_counter = -1;
+		else
+			philo[i].meal_counter = data->meal_counter;
+	}
+	return (philo);
 }
 
 void	ft_init_semaphores(t_data *data)
