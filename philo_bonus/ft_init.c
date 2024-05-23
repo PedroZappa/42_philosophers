25fedfd96ef06c17412b66d3b8ee6cdeb40c659b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 17:04:50 +0100

    philo_bonus/ft_init.c : ft_init_semaphores() implemented

--- philo_bonus/ft_init.c
+++ philo_bonus/ft_init.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:00:25 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/23 17:01:56 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/23 17:04:09 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,14 @@ t_philo	*ft_init_philo(t_data *data)
 
 void	ft_init_semaphores(t_data *data)
 {
-	(void)data;
+	sem_unlink("forks");
+	sem_unlink("death");
+	sem_unlink("msg");
+	sem_unlink("stop");
+	data->forks = sem_open("forks", O_CREAT, 0600, data->n_philos);
+	data->death = sem_open("death", O_CREAT, 0600, 1);
+	data->msg = sem_open("msg", O_CREAT, 0600, 1);
+	data->stop = sem_open("stop", O_CREAT, 0600, 1);
 }
 
 void	ft_fork_process(t_data *data, t_philo *philo)
