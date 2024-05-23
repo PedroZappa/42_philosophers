3646180e579850571fb82a350a264e4027b32ab1
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 22:24:37 +0100

    philo_bonus/ft_time.c : ft_philo_do()

--- philo_bonus/ft_time.c
+++ philo_bonus/ft_time.c
@@ -21,3 +21,22 @@ t_msec	ft_gettime(void)
 	gettimeofday(&t, NULL);
 	return ((t.tv_sec * 1000) + (t.tv_usec / 1000));
 }
+
+///
+/// @brief			Wait for a certain amount of time
+/// @param time		Time to wait in milliseconds
+/// @param data		Pointer to t_data struct
+/// @details		- Get current time in milliseconds
+/// 				- Wait until time is reached or data->done is set
+void	ft_philo_do(t_msec time, t_philo *philo)
+{
+	t_msec	t;
+
+	t = ft_gettime();
+	while (!philo->wait)
+	{
+		if ((ft_gettime() - t) >= time)
+			break ;
+		usleep(500);
+	}
+}
