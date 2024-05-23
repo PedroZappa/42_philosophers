c9e2b1687d7ee0750d11615280ed641047d26f12
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 16:31:42 +0100

    philo_bonus/ft_parse.c : ft_parse() implemented

--- philo_bonus/ft_parse.c
+++ philo_bonus/ft_parse.c
@@ -6,15 +6,49 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:52:25 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/23 15:53:14 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:30:23 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
+static int	ft_getnum(char *str);
+static int	ft_init_data(t_data *data, int n, int i);
+
 int	ft_parse(char **argv, t_data *data)
 {
-	(void)argv;
+	int	n;
+	int i;
+
+	i = 1;
+	while(argv[i])
+	{
+		n = ft_getnum(argv[i]);
+		if (n == -1)
+			return (1);
+		if (ft_init_data(data, n, i))
+			return (1);
+		++i;
+	}
+	if (i == 5)
+	{
+		data->meal_counter = 0;
+		data->curr_meal = 0;
+		data->max_meals = 0;
+	}
+	return (0);
+}
+
+static int	ft_getnum(char *str)
+{
+	(void)str;
+	return (-1);
+}
+
+static int	ft_init_data(t_data *data, int n, int i)
+{
 	(void)data;
+	(void)n;
+	(void)i;
 	return (0);
 }
--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 15:12:47 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/23 16:14:26 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:30:07 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ typedef struct s_data
 	int				time_to_sleep;
 	int				meal_counter;
 	int				max_meals;
-	int				current_meal;
+	int				curr_meal;
 	sem_t			*forks;
 	sem_t			*msg;
 	sem_t			*death;
@@ -98,6 +98,7 @@ typedef struct s_philo
 
 /// ft_error.c
 int		ft_perror(char *err);
+int		ft_perror_free(t_data *data, char *err, int ret);
 
 /// ft_free.c
 void	ft_free(t_data *data, t_philo *philo);
