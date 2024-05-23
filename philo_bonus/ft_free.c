80f49b620ced4532f5cd596039960dcf56394714
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-23 20:29:52 +0100

    philo_bonus/ft_free.c : Implemented

--- philo_bonus/ft_free.c
+++ philo_bonus/ft_free.c
@@ -6,14 +6,25 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:11:19 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/23 16:11:53 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/23 20:10:53 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
+/// @brief			Free all the allocated memory
+/// @param data		Philosophical data to be freed
+/// @param philo	Philosophers array to be freed
 void	ft_free(t_data *data, t_philo *philo)
 {
-	(void)data;
-	(void)philo;
+	int i;
+
+	i = 0;
+	while (i < data->n_philos)
+		kill(philo[i++].pid, SIGKILL);
+	sem_close(data->death);
+	sem_close(data->stop);
+	sem_close(data->msg);
+	sem_close(data->forks);
+	free(philo);
 }
