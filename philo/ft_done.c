c67fff4433a9ae30d8a9b9c207819c01df49179d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-29 19:39:47 +0100

    philo/ft_done.c : commented

--- philo/ft_done.c
+++ philo/ft_done.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:26:36 by passunca          #+#    #+#             */
-/*   Updated: 2024/05/29 18:57:33 by passunca         ###   ########.fr       */
+/*   Updated: 2024/05/29 19:37:33 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 /// @brief			Check if a philo dead
 /// @param philo	Pointer to a t_philo struct
 /// @return			TRUE if dead, FALSE if not
+/// @details		- Lock a mutex
+/// 				- Check if the philo died
+/// 				- Unlock the mutex
 /// @note			Used in ft_start_philo & ft_log
 int	ft_isdead(t_philo *philo)
 {
@@ -31,6 +34,9 @@ int	ft_isdead(t_philo *philo)
 /// @brief			Check if the simulation is done
 /// @param philo	Pointer to a t_philo struct
 /// @return			TRUE if done, FALSE if not
+/// @details		- Lock a mutex
+/// 				- Check if the simulation is done
+/// 				- Unlock the mutex
 /// @note			Used in ft_eating & ft_log
 int	ft_isdone(t_philo *philo)
 {
@@ -46,6 +52,9 @@ int	ft_isdone(t_philo *philo)
 
 /// @brief			Set a philo dead
 /// @param data		Pointer to a t_data struct
+/// @details		- Lock a mutex
+/// 				- Set the died flag
+/// 				- Unlock the mutex
 /// @note			Used in ft_monitor
 void	ft_died(t_data *data)
 {
@@ -61,6 +70,12 @@ void	ft_died(t_data *data)
 /// @var done			Number of philos who have eaten n_meals
 /// @var meals_count	Number of meals eaten by a philo
 /// @return				TRUE if done, FALSE if not
+/// @details			Loop through the array of philos
+///						- Lock a mutex
+///						- Get the number of meals eaten
+///						- Unlock the mutex
+///						- Check if n_meals have been eaten
+///						- Increment done if n_meals have been eaten by a philo
 /// @note				Used in ft_monitor
 int	ft_are_done(t_philo *philo, t_data *data)
 {
@@ -87,6 +102,9 @@ int	ft_are_done(t_philo *philo, t_data *data)
 
 /// @brief			Set the simulation as done
 /// @param data		Pointer to a t_data struct
+/// @details		- Lock a mutex
+/// 				- Set the done flag
+/// 				- Unlock the mutex
 /// @note			Used in ft_monitor
 void	ft_done(t_data *data)
 {
